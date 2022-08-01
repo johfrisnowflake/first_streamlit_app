31faa671e033cb24c480750bbe48531e6f7534a1
Joh Fri <110387833+johfrisnowflake@users.noreply.github.com>
2022-08-01 12:05:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit
 
 streamlit.title('My Parents New Helthy Diner')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('My Parents New Helthy Diner')
+
+
+
+
